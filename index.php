90fd09c2c5f8939686fa99ec4afb4df63fd7bb32
Unknown <hn85412404@gmail.com>
2018-09-15 18:25:04 +0800

    0915

--- index.php
+++ index.php
@@ -6,21 +6,47 @@
 <!--{$smarty.const.ROOT_URLPATH}-->
 
 
-<script type="text/javascript" src="https://www.manara.asia/tw/js/products.js"></script>
+<!-- <link rel="stylesheet" href="https://www.manara.asia/tw/user_data/packages/defaultta/css/new_lee.css" /> -->
+<link rel="stylesheet" href="css/new_lee.css" />
+
+
+
 <div id="container">
     <div id="one_maincolumn" class="main_column">
         <!--   肚子開始  -->
         <div class="banner-slider">
-                    <div class="mm-tabs-wrapper">
-                        <div class="tab-item">
-                            <ul class="bxslider">                             
-                                <li><a href="https://www.manara.asia/tw/lp6cg/?utm_source=RU&utm_medium=official_cg&utm_campaign=AAA1705004"><img src="https://www.manara.asia/tw/user_data/new/img/banner/manara_1024x522_8.jpg" /></a></li>
-                                <li><a href="https://www.manara.asia/tw/user_data/regular.php" target="_blank"><img src="https://www.manara.asia/tw/user_data/new/img/banner/manara_1024x522_9.jpg" /></a></li>
-                              
-                            </ul>
-                        </div>
-                    </div>
+            <div class="mm-tabs-wrapper">
+                <div class="tab-item">
+                    <ul class="bxslider">                             
+                        <li><a href="https://www.manara.asia/tw/lp6cg/?utm_source=RU&utm_medium=official_cg&utm_campaign=AAA1705004"><img src="https://www.manara.asia/tw/user_data/new/img/banner/manara_1024x522_8.jpg" /></a></li>
+                        <li><a href="https://www.manara.asia/tw/user_data/regular.php" target="_blank"><img src="https://www.manara.asia/tw/user_data/new/img/banner/manara_1024x522_9.jpg" /></a></li>
+                    </ul>
                 </div>
+            </div>
+        </div>
+
+        <div class="W1280 columns-12">
+            <div class="col-l-12">
+                <div id="attention"><h5>服務異動通知</h5></div>
+                
+            </div>
+        </div>
+
+        <div id="special" class="W1280 columns-12">
+            <h5 class="title">special contents<span class="sub_title">今日のスペシャル</span></h5>
+            
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <a href="https://www.manara.asia/tw/lp6cg/?utm_source=RU&amp;utm_medium=official_cg&amp;utm_campaign=AAA1705004">
+                    <img src="https://www.manara.asia/tw/user_data/new/img/link-banner-10.jpg" width="100%">
+                </a>
+            </div>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <a href="https://www.manara.asia/tw/user_data/regular.php" target="_blank">
+                <img src="https://www.manara.asia/tw/user_data/new/img/link-banner-11.jpg" width="100%">
+                </a>
+            </div>
+        </div>
+
 
 
 
