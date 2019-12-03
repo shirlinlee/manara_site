1b990cb6a57c45b3ed934a6565770082da380e80
Unknown <hn85412404@gmail.com>
2019-12-04 00:21:52 +0800

    [update] bb product 50%

--- user_data/new_201811/js/init.js
+++ user_data/new_201811/js/init.js
@@ -1,13 +1,10 @@
-var dev = window
-    .location
-    .href
-    .indexOf('localhost') > -1,
-    domain = '',
-    $body = (window.opera) ?
-    (document.compatMode == "CSS1Compat" ?
-        $('html') :
-        $('body')) :
-    $('html,body');
+var dev = window.location.href.indexOf("localhost") > -1,
+  domain = "",
+  $body = window.opera
+    ? document.compatMode == "CSS1Compat"
+      ? $("html")
+      : $("body")
+    : $("html,body");
 
 // document.addEventListener('DOMContentLoaded', function () {     var tag =
 // location.hash;     if (tag === "#howtouse") {         console.log(tag,
@@ -17,61 +14,58 @@ var dev = window
 // history.replaceState(null, null, ' ');     } });
 
 $(function() {
-    var tag = location.hash;
-    if (dev) {
-        domain = 'https://ecweb-dev.cros.tw/tw/user_data/';
-    }
+  var tag = location.hash;
+  if (dev) {
+    domain = "https://ecweb-dev.cros.tw/tw/user_data/";
+  }
 
-    if (tag === "#howtouse") {
-        $(window)
-            .on('load', function() {
-                use();
-            });
-        use();
-
-    }
-
-    function use() {
-        setTimeout(function() {
-            $('html,body').animate({
-                scrollTop: $(".sec6")
-                    .offset()
-                    .top - 90
-            }, 'slow');
-            history.replaceState(null, null, ' ');
-        }, 1000)
-    }
-
-    $('body')
-        .on('click', '.info', function() {
-            window.location = domain + "regular.php";
-        });
-
-    $('body').on('click', '.all_', function() {
-        window.location = "product_list.php";
+  if (tag === "#howtouse") {
+    $(window).on("load", function() {
+      use();
     });
+    use();
+  }
 
-    // $('.sec4').on('click', 'h3', function () {     window.location = domain +
-    // "features.php"; });
+  function use() {
+    setTimeout(function() {
+      $("html,body").animate(
+        {
+          scrollTop: $(".sec6").offset().top - 90
+        },
+        "slow"
+      );
+      history.replaceState(null, null, " ");
+    }, 1000);
+  }
 
-    $('.sec9').on('click', 'li', function() {
-        var page = $(this).attr('data-href');
-        window.location = domain + page;
-    });
+  $("body").on("click", ".info", function() {
+    window.location = domain + "regular.php";
+  });
 
-    var name = $('.p_name').html();
-    var bread = '<div class="W1200 bread"> <span class = "f15 f_red f_left" ><a href="/tw/">首頁 / </' +
-        'a><a href="product_list.php">MANARA商品 / </a>' + name + '</span></div>';
+  $("body").on("click", ".all_", function() {
+    window.location = "product_list.php";
+  });
 
-    $('#_product').prepend(bread);
+  // $('.sec4').on('click', 'h3', function () {     window.location = domain +
+  // "features.php"; });
 
-    $('.day_night').on('click', 'li', function() {
-        var page = $(this).attr('data-href');
-        if (page === undefined) {
-            console.log('now');
-            return;
-        }
-        window.location = page;
-    });
+  $(".sec9").on("click", "li", function() {
+    var page = $(this).attr("data-href");
+    window.location = domain + page;
+  });
 
-})
+  var name = $(".p_name").html();
+  // var bread = '<div class="W1200 bread"> <span class = "f15 f_red f_left" ><a href="/tw/">首頁 / </' +
+  //     'a><a href="product_list.php">MANARA商品 / </a>' + name + '</span></div>';
+
+  // $('#_product').prepend(bread);
+
+  $(".day_night").on("click", "li", function() {
+    var page = $(this).attr("data-href");
+    if (page === undefined) {
+      console.log("now");
+      return;
+    }
+    window.location = page;
+  });
+});
